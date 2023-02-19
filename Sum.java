ece88740c134c83f5d4fc1db3d7bd34bf7f1e62e
20P31A0497 <84895813+20P31A0497@users.noreply.github.com>
2023-02-19 17:04:12 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Sum{
+    public static void main(String[] args) {
+        int[] arr = new int[] { 25, 35, 12, 4, 36, 48 };
+        int sum = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            list.add(sum);
+        }
+        System.out.println(list);
+    }
+}
